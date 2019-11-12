92db080a99c48493e7c77898c099aa6bb50b70bd
MadMind <support@ntinformatique.ca>
2019-11-12 10:02:36 -0500

    Initial Dev Commit

--- TP3Environnements/Controllers/HomeController.cs
+++ TP3Environnements/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace TP3Environnements.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
