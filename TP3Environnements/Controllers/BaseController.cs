cc66a8a14e02449ebc8a709bae606600ab2bf027
MadMind <support@ntinformatique.ca>
2019-11-12 11:35:08 -0500

    commit 9

--- TP3Environnements/Controllers/BaseController.cs
+++ TP3Environnements/Controllers/BaseController.cs
@@ -11,7 +11,7 @@ namespace TP3Environnements.Controllers
     {
         [HttpPost]
         [AllowAnonymous]
-        public ActionResult SetCulture(string culture)
+        public void SetCulture(string culture)
         {
             // En utilisant la session
             Session["environnementCourant"] = culture;
